425542993cac4534bf5ce346fd4a25950e9340ea
김준성 <37741469+jikukjs@users.noreply.github.com>
2025-07-19 18:51:01 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,11 @@
-n = int(input())
-n_list = set(map(int,input().split()))
-m = int(input())
-m_list=list(map(int,input().split()))
+N = int(input())
+N_list = set(map(int,input().split()))
 
-for w in m_list:
-    if w in n_list:
-        print("1")
-    else :
-        print("0")
+M = int(input())
+M_list = list(map(int,input().split()))
+
+for m in M_list:
+    if m in N_list:
+        print(1)
+    else:
+        print(0) 
